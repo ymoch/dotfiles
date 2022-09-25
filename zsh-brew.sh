1999a23a9a274bb264a06a5f20943aabdb66bd26
Yu Mochizuki <ymoch@users.noreply.github.com>
2022-09-25 17:47:23 +0900

    Create zsh-brew.sh

---
+++ zsh-brew.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+
+brew install \
+  vim \
+  coreutils \
+  findutils \
+  gnu-sed \
+  gnu-tar \
+  grep \
+  \
+  zlib \
+  bzip2 \
+  libffi
+
+brew install pyenv
+brew install nvm
+brew install jenv
